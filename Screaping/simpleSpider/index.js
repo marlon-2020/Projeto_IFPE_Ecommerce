5bff5093f8e5aa9b319c088c7cff311493e66c85
Marlon Silva <71356608+marlon-2020@users.noreply.github.com>
2020-11-23 21:12:20 -0300

    Atualização do screaping
    Agora o Screaper faz download de imagens dos sites, controle dos seletores dos sites é necessário no back-end.

--- Screaping/simpleSpider/index.js
+++ Screaping/simpleSpider/index.js
@@ -1,4 +1,7 @@
 const mysql=require('mysql2');
+const download = require('image-downloader');
+const { link } = require('fs');
+
 
 var Crawler = {
 	request : null,
@@ -11,7 +14,7 @@ var Crawler = {
 		Crawler.getProducts();
 	},
 	getProducts: function(){
-		Crawler.request('https://www.magazineluiza.com.br/busca/cal%C3%A7ados/11/', function(err, res, body){
+		Crawler.request('https://www.magazineluiza.com.br/busca/cal%C3%A7ados/2/', function(err, res, body){
 			if(err)
 				console.log('Error: ' + err);
 			var $ = Crawler.cheerio.load(body);
@@ -19,8 +22,19 @@ var Crawler = {
 				var produto  = $(this).find('.productTitle').text().trim().replace(/^\s+|\s+$/g, "");
                 var valor = $(this).find('.productPrice .price').text().trim().replace(/^\s+|\s+$/g, "");
                 var valorOri = $(this).find('.productPrice .originalPrice').text().trim().replace(/^\s+|\s+$/g, "");
-                var valorPar = $(this).find('.productPrice .installmentPrice').text().trim().replace(/^\s+|\s+$/g, "");
-				save(produto, valor, valorOri, valorPar,);
+				var valorPar = $(this).find('.productPrice .installmentPrice').text().trim().replace(/^\s+|\s+$/g, "");
+				var imagem = $(this).find('.alignment-image .product-image').attr('data-original');
+				save(produto, valor, valorOri, valorPar, imagem);
+				console.log(imagem)
+				const options = {
+					url: `${imagem}`,
+					dest: '/IFPE/Projeto/PeakyPay/Screaping/simpleSpider/imagens'                
+				}
+				download.image(options)
+  					.then(({ filename }) => {
+    					console.log('Saved to', filename)  // saved to /path/to/dest/image.jpg
+  					})
+				  	.catch((err) => console.error(err))
 			});
 			console.log('Terminado!');
 		});
@@ -36,11 +50,16 @@ const connection = mysql.createConnection({
     password: 'msf131906'
  });
     
-function save(produto, valor, valorOri, valorPar){
+function save(produto, valor, valorOri, valorPar, imagem){
     connection.query(
-    	'insert into produtos (nomeProduto, valor, valorOri, valorPar) values (?,?,?,?)', [produto, valor, valorOri, valorPar],        
+    	'insert into produtos (nomeProduto, valor, valorOri, valorPar, imagem) values (?,?,?,?,?)', [produto, valor, valorOri, valorPar, imagem],        
     );
  }
+
+
+ 
+
+ 
 
 
    
